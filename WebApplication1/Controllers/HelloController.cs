d3f671b596a2f633f3cb7b77fcd32d904359ac26
mudasirshah434 <mudasirshah9777@gmail.com>
2025-02-02 18:09:58 +0500

    Add project files.

---
+++ WebApplication1/Controllers/HelloController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BasicApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HelloController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new { message = "Hello, Mudasir! Welcome to .NET API!" });
+        }
+    }
+}
